d95567333477cc067f5564f822da23e5b6241dd8
kyeongin- <a27313797@gmail.com>
2023-07-19 14:38:08 +0900

    Feat/BE : Category Repository 생성
    Jpa Category Repository 생성

---
+++ backend/src/main/java/com/ssafy/common/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.common.repository;
+
+import com.ssafy.common.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
