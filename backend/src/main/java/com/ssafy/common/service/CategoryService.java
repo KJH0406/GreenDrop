c7421e966bf995da365d0785ce3cbe91cc4d719f
kyeongin- <a27313797@gmail.com>
2023-07-19 14:40:11 +0900

    Feat/BE : Category Service 생성
    category를 등록 , 조회 하는 메소드 생성

---
+++ backend/src/main/java/com/ssafy/common/service/CategoryService.java
@@ -0,0 +1,31 @@
+package com.ssafy.common.service;
+
+import com.ssafy.common.dto.request.CategoryRequestDto;
+import com.ssafy.common.entity.Category;
+import com.ssafy.common.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public void categorySave(CategoryRequestDto categoryRequestDto){
+        Category category = categoryRequestDto.toEntity();
+
+        categoryRepository.save(category);
+    }
+
+
+    public List<Category> categoryList(){
+        List<Category> categories = categoryRepository.findAll();
+        return categories;
+    }
+
+}
