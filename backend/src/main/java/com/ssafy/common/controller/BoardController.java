e0926a6d466b6db2db7259d1ba2c3ac6a10b63f9
kyeongin- <a27313797@gmail.com>
2023-07-20 16:39:45 +0900

    Feat/BE : BoardContorller 작성
    - PostMapping 을 통하여 게시글을 등록 해 줄 registBoard 메소드 작성 -> @ResponseBody 의 BoardDto 형태로 데이터를 받음
    - GetMapping 을 통하여 게시글의 상세페이지를 조회 해 줄 detailBoardPage 메소드 작성 -> @PathVariable 의 boardNo 값으로 게시글의 PK 번호를 받아와 해당하는 게시글 조

--- backend/src/main/java/com/ssafy/common/controller/BoardController.java
+++ backend/src/main/java/com/ssafy/common/controller/BoardController.java
@@ -1,4 +1,41 @@
 package com.ssafy.common.controller;
 
+import com.ssafy.common.dto.BoardDto;
+import com.ssafy.common.dto.response.BoardResponseDto;
+import com.ssafy.common.security.UserIp;
+import com.ssafy.common.service.BoardService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/board")
+@RestController
 public class BoardController {
+
+    private final BoardService boardService;
+    private final UserIp userIp;
+
+    @PostMapping("/regist")
+    public ResponseEntity<?> registBoard(
+            @RequestBody BoardDto boardDto,
+            HttpServletRequest request
+    ){
+        String ipAdress = userIp.searchIP(request);
+        boardService.saveBoard(boardDto,ipAdress);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{boardNo}")
+    public ResponseEntity<?> detailBoardPage(@PathVariable Long boardNo){
+        BoardResponseDto boardResponseDto  = boardService.detailBoardPage(boardNo);
+
+        return new ResponseEntity<>(boardResponseDto,HttpStatus.OK);
+    }
+
 }
