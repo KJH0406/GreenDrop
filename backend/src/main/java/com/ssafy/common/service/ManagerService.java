8ba1e01e7d32beddaeccc7239c94da2ac4c63592
WonjunChun <chunjh1103@naver.com>
2023-07-24 17:13:01 +0900

    Feat/BE : ManagerService에 패스워드 암호화 적용
    - 관리자 등록 시 패스워드 암호화 하여 저장하도록 encoder 추가
    - 로그인 시 기존에 저장된 암호화된 패스워드와 요청으로 받은 패스워드 비교

--- backend/src/main/java/com/ssafy/common/service/ManagerService.java
+++ backend/src/main/java/com/ssafy/common/service/ManagerService.java
@@ -4,6 +4,7 @@ import com.ssafy.common.dto.request.ManagerRequestDto;
 import com.ssafy.common.dto.response.ManagerResponseDto;
 import com.ssafy.common.entity.Manager;
 import com.ssafy.common.repository.ManagerRepository;
+import com.ssafy.common.security.Encoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,21 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ManagerService {
     private final ManagerRepository managerRepository;
-    //TODO: jwt 토큰, 패스워드 암호화 추후 추가
+    //TODO: jwt 토큰 추후 추가
     @Transactional
     public ManagerResponseDto registManager(ManagerRequestDto request){
-        //TODO: 패스워드 암호화 적용
-        Manager manager = managerRepository.save(request.toRegistEntity());
+        Encoder encoder = new Encoder();
+        Manager manager = managerRepository.save(request.toRegistEntity(encoder)); //패스워드 암호화를 위한 encoder 추가
 
         return ManagerResponseDto.from(manager);
-
     }
 
     //TODO: 로그인 시각 DB에 저장하기
     public ManagerResponseDto signIn(ManagerRequestDto request){
+        Encoder encoder = new Encoder();
         Manager manager = managerRepository.findById(request.id())
-                //TODO: 암호화 적용 후 암호화된 비밀번호와 비교하도록 수정
-                .filter(it -> it.getPassword().equals(request.password()))
+                .filter(it -> encoder.matches(request.password(), it.getPassword())) //요청에 담긴 password와 암호화된 password 비교
                 .orElseThrow(()-> new IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다."));
         //TODO: 토큰 생성
         //TODO: 반환 시 from에 토큰 추가
