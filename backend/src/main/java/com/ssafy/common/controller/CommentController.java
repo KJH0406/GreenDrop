174766b8470dfdef217846fddd4c03504dd6a27f
kyeongin- <a27313797@gmail.com>
2023-07-25 17:52:10 +0900

    Feat/BE : 댓글 등록 Service Controller 코드 작성
    -  일반 댓글의 등록 service 와 controller 코드 작성

--- backend/src/main/java/com/ssafy/common/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/common/controller/CommentController.java
@@ -1,4 +1,34 @@
 package com.ssafy.common.controller;
 
+import com.ssafy.common.dto.CommentDto;
+import com.ssafy.common.security.UserIp;
+import com.ssafy.common.service.CommentService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+@RestController
 public class CommentController {
+
+    private final CommentService commentService;
+    private final UserIp userIp;
+
+    @PostMapping("/regist/{boardNo}")
+    ResponseEntity<?> registParentComment(
+            @PathVariable Long boardNo,
+            @RequestBody CommentDto commentDto,
+            HttpServletRequest request
+            ){
+
+        String ipAdress = userIp.searchIP(request);
+        commentService.saveParentComment(boardNo,ipAdress,commentDto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
