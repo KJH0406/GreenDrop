0e775687768b43e32563d4c693dd73f759b6dfec
kyeongin- <a27313797@gmail.com>
2023-07-25 09:16:44 +0900

    Feat/BE : 게시판 리스트 조회를 위한 query 작성
    - QueryDsl을 사용하여 게시판리스트를 전체 조회 할 수 있는 쿼리문 작성
    - 게시글의 좋아요 내림차순으로 정렬

--- backend/src/main/java/com/ssafy/common/repository/BoardRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/common/repository/BoardRepositoryImpl.java
@@ -1,12 +1,25 @@
 package com.ssafy.common.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.common.dto.response.BoardResponseDto;
 import com.ssafy.common.entity.Board;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
-import static com.ssafy.common.entity.QBoard.board;
+import java.util.List;
 
+import static com.ssafy.common.entity.QBoard.board;
+import static com.ssafy.common.entity.QBoardCategory.boardCategory;
+import static com.ssafy.common.entity.QCategory.category;
+
+@Slf4j
 @Repository
 public class BoardRepositoryImpl extends QuerydslRepositorySupport implements BoardRepositoryCustom{
     private final JPAQueryFactory queryFactory;
@@ -22,6 +35,24 @@ public class BoardRepositoryImpl extends QuerydslRepositorySupport implements Bo
                 .set(board.likeCount, board.likeCount.add(1))
                 .where(board.eq(boardEntity))
                 .execute();
+    }
+
+    @Override
+    public Page<BoardResponseDto> allBoardList(Pageable pageable) {
+
+        JPQLQuery<BoardResponseDto> query = queryFactory
+                .select(Projections.fields(BoardResponseDto.class,
+                board.question,board.leftAnswer,board.rightAnswer, board.ip,board.likeCount
+                        ,board.nickname,board.lastModifiedDate,category.item))
+                .from(board)
+                .leftJoin(board.boardCategories,boardCategory)
+                .leftJoin(boardCategory.category,category)
+                .where(board.isDeleted.eq(0))
+                .orderBy(board.likeCount.desc());
+
+
+        List<BoardResponseDto> boardList = this.getQuerydsl().applyPagination(pageable, query).fetch();
+        return new PageImpl<BoardResponseDto>(boardList, pageable, query.fetchCount());
     }
 
 }
