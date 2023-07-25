ff4340ee295bc0d4620465ac775a064aaa7689fa
김장호 <rlawkdgh0406@naver.com>
2023-07-25 10:32:07 +0900

    Rename/FE : 썸네일 오타 수정

--- frontend/src/pages/Intro.js
+++ frontend/src/pages/Intro.js
@@ -1,10 +1,10 @@
-import Tumnail from "../components/Intro/Tumnail";
+import Thumbnail from "../components/Intro/Thumbnail";
 import classes from "./Intro.module.css";
 
 function IntroPage() {
   return (
     <div className={classes.introContainer}>
-      <Tumnail />
+      <Thumbnail />
     </div>
   );
 }
