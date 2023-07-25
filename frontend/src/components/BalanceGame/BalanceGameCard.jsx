f60a36f8aa7d55c49acea8e68d5535183b80a44c
이대건 <ldg031@naver.com>
2023-07-25 10:11:54 +0900

    Feat/FE: 밸런스 게임 게시판 리스트 스타일적용
    BalenceGameCard.jsx, BalanceGameCard.jsx, BalanceGameCardList.jsx에 스타일 적용 완료

--- frontend/src/components/BalanceGame/BalanceGameCard.jsx
+++ frontend/src/components/BalanceGame/BalanceGameCard.jsx
@@ -1,24 +1,59 @@
 import classes from "./BalanceGameCard.module.css"
+import menuImg from "../../assets/list_menu.png"
+import fullHeartImg from "../../assets/full_heartpng.png"
+import emptyHeartImg from "../../assets/empty_heartpng.png"
+import commentImg from "../../assets/commentpng.png"
+import { useState } from "react"
 function BalanceGameCard() {
+  let [likeCnt, setLikeCnt] = useState(0)
+  let [commentCnt, setCommentCnt] = useState(0)
+  let [category] = useState("#스포츠")
   return (
     <div className={classes.outer_box}>
       <div className={classes.top}>
-        <div className={classes.top_item_left}>user1</div>
-        <div className={classes.top_item_right}>메뉴</div>
+        <div className={`${classes.top_item_left} ${classes.title}`}>user1</div>
+        {/* <div className={classes.top_item_right}></div> */}
+        <input
+          type="image"
+          src={menuImg}
+          alt="menu"
+          className={`${classes.top_item_right} ${classes.image}`}
+          onClick={() => {}}
+        />
       </div>
       <div className={classes.top}>
-        <div className={classes.top_item_left}>메시 vs 호날두</div>
+        <div className={`${classes.top_item_left} ${classes.subject}`}>
+          메시 vs 호날두aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
         <div className={classes.top_item_right}>2023.07.24</div>
       </div>
       <div className={classes.middle}>
-        <div className={classes.middle_item}>지문1</div>
-        <div className={classes.middle_item}>지문2</div>
+        <div className={`${classes.middle_item} ${classes.middle_item_left}`}>
+          지문1
+        </div>
+        <div className={`${classes.middle_item} ${classes.middle_item_right}`}>
+          지문2
+        </div>
       </div>
       <div className={classes.bottom}>
         <div className={classes.bottom_left}>
-          좋아요, 좋아요 수, 댓글, 댓글 수
+          <input
+            type="image"
+            src={emptyHeartImg}
+            className={classes.bottom_image}
+            onClick={() => {}}
+          />
+          <span>{likeCnt}</span>
+
+          <input
+            type="image"
+            src={commentImg}
+            className={classes.bottom_image}
+            onClick={() => {}}
+          />
+          <span>{commentCnt}</span>
         </div>
-        <div className={classes.bottom_right}>카테고리</div>
+        <div className={classes.bottom_right}>{category}</div>
       </div>
     </div>
   )
--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -1,23 +1,36 @@
 import classes from "./Board.module.css"
 import BalanceGameList from "../components//BalanceGame/BalanceGameList"
+import BalanceGameCategoryList from "../components/BalanceGame/BalanceGameCategoryList"
+import search from "../assets/search.png"
+import star from "../assets/star.png"
 function BoardPage() {
   return (
     <div className={classes.outer_box}>
-      <h1>Green Balance Game</h1>
+      {/* <h1>Green Balance Game</h1> */}
+      <div className={classes.title}>
+        <h2 className={classes.first_word}>Green</h2>
+        <h2 className={classes.second_word}>Balance Game</h2>
+      </div>
       <div className={classes.row} id={classes.search}>
-        <input type="text" className="" />
-        <button>검색</button>
+        <span className={classes.search_area}>
+          <input type="text" className={classes.search_input} />
+          <input
+            type="image"
+            src={search}
+            alt="search"
+            className={classes.search_btn}
+            onClick={() => {}}
+          />
+        </span>
       </div>
       <div className={classes.row}>
-        <button>밸런스 게임 등록하기</button>
+        <button className={classes.regist_btn}>밸런스 게임 등록하기</button>
       </div>
       <div className={classes.row}>
-        <div className={classes.left_align}>추천 카테고리</div>
-        <div className={classes.recommend}>
-          스포츠 인기순스포츠 인기순스포츠 인기순스포츠 인기순스포츠
-          인기순스포츠 인기순스포츠 인기순스포츠 인기순스포츠 인기순스포츠
-          인기순스포츠 인기순스포츠 인기순스포츠 인기순
+        <div className={classes.left_align}>
+          추천 카테고리 <img src={star} alt="star"></img>
         </div>
+        <BalanceGameCategoryList />
       </div>
       {/* 글 리스트만 컴포넌트로  */}
       {/* 검색시 게임 리스트 state만 바꿔주면 아랑서 화면 출력될 듯(다시 전체 글로는 어떻게 돌아가지?) */}
