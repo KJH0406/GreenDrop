9207e960c72e239938f7763349d4f43e97c44bab
kyeongin- <a27313797@gmail.com>
2023-07-25 13:28:05 +0900

    Refactor/BE : repository 반환값 변경
    findByItem의 반환값을 Category에서 Optional<Category>로 변경

--- backend/src/main/java/com/ssafy/common/repository/CategoryRepository.java
+++ backend/src/main/java/com/ssafy/common/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.common.repository;
 import com.ssafy.common.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
-    Category findByItem(String item);
+    Optional<Category> findByItem(String item);
 }
