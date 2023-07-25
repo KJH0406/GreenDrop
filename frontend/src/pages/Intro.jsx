bec4fe93b00555efb7202fbac20b0c87d7330717
김장호 <rlawkdgh0406@naver.com>
2023-07-25 16:12:37 +0900

    Feat/FE : 서비스 소개 채팅 구현
    Green Drop 소개하는 말풍선 채팅 구현

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,10 +1,12 @@
 import Thumbnail from "../components/Intro/Thumbnail";
+import ServiceIntroChat from "../components/Intro/ServiceIntroChat";
 import classes from "./Intro.module.css";
 
 function IntroPage() {
   return (
     <div className={classes.introContainer}>
       <Thumbnail />
+      <ServiceIntroChat />
     </div>
   );
 }
