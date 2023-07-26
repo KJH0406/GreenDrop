27d3dfe4baf48c5bfbe3b247b33715be320e0672
이대건 <ldg031@naver.com>
2023-07-26 10:22:34 +0900

    Feat/FE: 글 등록 폼 구현
    BalanceGameWriteForm을 페이지로 사용하여 글 등록 폼 구현 완료

--- frontend/src/components/BalanceGame/BalanceGameCard.jsx
+++ frontend/src/components/BalanceGame/BalanceGameCard.jsx
@@ -1,13 +1,16 @@
 import classes from "./BalanceGameCard.module.css"
 import menuImg from "../../assets/list_menu.png"
-import fullHeartImg from "../../assets/full_heartpng.png"
+// import fullHeartImg from "../../assets/full_heartpng.png"
 import emptyHeartImg from "../../assets/empty_heartpng.png"
 import commentImg from "../../assets/commentpng.png"
 import { useState } from "react"
 function BalanceGameCard() {
-  let [likeCnt, setLikeCnt] = useState(0)
-  let [commentCnt, setCommentCnt] = useState(0)
+  // let [likeCnt, setLikeCnt] = useState(0)
+  // let [commentCnt, setCommentCnt] = useState(0)
+  let [likeCnt] = useState(0)
+  let [commentCnt] = useState(0)
   let [category] = useState("#스포츠")
+
   return (
     <div className={classes.outer_box}>
       <div className={classes.top}>
@@ -29,10 +32,27 @@ function BalanceGameCard() {
       </div>
       <div className={classes.middle}>
         <div className={`${classes.middle_item} ${classes.middle_item_left}`}>
-          지문1
+          <div className={classes.answer}>
+            왼쪽 지문 왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽
+            지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽
+            지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽
+            지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽
+            지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽 지문왼쪽
+            지문왼쪽 지문왼쪽 지문왼쪽 지문
+          </div>
+          <div className={classes.cover_bar}></div>
         </div>
         <div className={`${classes.middle_item} ${classes.middle_item_right}`}>
-          지문2
+          <div className={classes.answer}>
+            오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽 지문오른쪽
+            지문오른쪽 지문
+          </div>
+          <div className={classes.cover_bar}></div>
         </div>
       </div>
       <div className={classes.bottom}>
@@ -40,18 +60,18 @@ function BalanceGameCard() {
           <input
             type="image"
             src={emptyHeartImg}
-            className={classes.bottom_image}
-            onClick={() => {}}
+            className={classes.bottom_like}
+            alt=""
           />
-          <span>{likeCnt}</span>
+          <span className={classes.bottom_like_count}>{likeCnt}</span>
 
           <input
             type="image"
             src={commentImg}
-            className={classes.bottom_image}
-            onClick={() => {}}
+            className={classes.bottom_comment}
+            alt=""
           />
-          <span>{commentCnt}</span>
+          <span className={classes.bottom_comment_count}>{commentCnt}</span>
         </div>
         <div className={classes.bottom_right}>{category}</div>
       </div>
