205d53033e8a41ed94de1598d26d9c1640243606
WonjunChun <chunjh1103@naver.com>
2023-07-26 13:54:27 +0900

    Feat/BE : Game Entity 작성

---
+++ backend/src/main/java/com/ssafy/common/entity/Game.java
@@ -0,0 +1,53 @@
+package com.ssafy.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="game")
+public class Game implements Comparable<Game>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_seq")
+    private Long gameSeq;
+
+    //FK
+    @ManyToOne(targetEntity = Board.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_seq", referencedColumnName = "board_seq")
+    private Board board;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "left_answer")
+    private String leftAnswer;
+
+    @Column(name = "right_answer")
+    private String rightAnswer;
+
+    private LocalDate date;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "left_amount")
+    private Integer leftCount;
+
+    @Column(name = "right_amount")
+    private Integer rightCount;
+
+
+    @Override
+    public int compareTo(Game o) {
+        return this.date.compareTo(o.date);
+    }
+}
