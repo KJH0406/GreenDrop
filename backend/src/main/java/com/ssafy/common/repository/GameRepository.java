1bda701299b758aeecd4186005661a4dd9d5e3e0
WonjunChun <chunjh1103@naver.com>
2023-07-26 16:40:00 +0900

    Feat/BE : Game Repository 생성
    - JPARepository를 상속받아 Game Entity에 대한 Repository 생성

--- backend/src/main/java/com/ssafy/common/repository/GameRepository.java
+++ backend/src/main/java/com/ssafy/common/repository/GameRepository.java
@@ -1,7 +1,5 @@
 package com.ssafy.common.repository;
 
-
-import com.ssafy.common.entity.Board;
 import com.ssafy.common.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,9 +8,8 @@ import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     //TODO: GameRepositoryImpl에서 QueryDsl을 통해 상세 쿼리 작성하기?
-    Game findGameByDate(LocalDate date); //등록 일자를 통해 게임 조회
-
-    Board findByBoard_BoardSeq(Long boardSeq);
+    Game findOneByDate(LocalDate date); //등록 일자를 통해 게임 조회
+    
 
     List<Game> findAll();
 
