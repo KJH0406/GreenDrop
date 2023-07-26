8b1c18d720bfb00693ecea1436d9a61bdef4360f
WonjunChun <chunjh1103@naver.com>
2023-07-26 14:05:37 +0900

    Feat/BE : Game Controller 작성
    - PostMapping을 통해 밸런스게임 등록해 줄 registGame 작성 -> @PathVariable의 Long 형태로 boardNo 받음
    - GetMapping을 통해 전체 밸런스게임 리스트 조회하는 allGameList 작성
    - GetMapping을 통해 특정 날짜의 밸런스게임 조회하는 getGameByDate 작성 -> @PathVariable의 LocalDate 형태로 date 받음

---
+++ backend/src/main/java/com/ssafy/common/controller/GameController.java
@@ -0,0 +1,37 @@
+package com.ssafy.common.controller;
+
+import com.ssafy.common.entity.Game;
+import com.ssafy.common.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/game")
+@RestController
+public class GameController {
+    private final GameService gameService;
+
+    @PostMapping("/regist/{boardNo}")
+    public ResponseEntity<Object> registGame(@PathVariable Long boardNo){
+        gameService.registGame(boardNo);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List> allGameList(){
+        List<Game> gameList = gameService.findAllGameList();
+
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{date}")
+    public ResponseEntity<Game> getGameByDate(@PathVariable LocalDate date){
+        Game game = gameService.findGameByDate(date);
+        return new ResponseEntity<>(game, HttpStatus.OK);
+    }
+}
