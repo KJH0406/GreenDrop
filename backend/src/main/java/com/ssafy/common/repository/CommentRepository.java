9f487dc432fddeedcf50067460b0832185801443
kyeongin- <a27313797@gmail.com>
2023-07-27 09:51:40 +0900

    Feat/BE : 댓글 관련 Querydsl 추가
    - commentRepository에 QueryDsl 사용을 위한 extends 추가
    - QComment 파일 추가
    - commentRepositoryCustom 파일 추가

--- backend/src/main/java/com/ssafy/common/repository/CommentRepository.java
+++ backend/src/main/java/com/ssafy/common/repository/CommentRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.common.repository;
 import com.ssafy.common.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment,Long>, CommentRepositoryCustom {
 }
---
+++ backend/src/main/java/com/ssafy/common/repository/CommentRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.ssafy.common.repository;
+
+import com.ssafy.common.entity.Comment;
+
+import java.util.List;
+
+public interface CommentRepositoryCustom {
+
+    List<Comment> findByComment(Long boardNo);
+    List<Comment> findByParentId(Long parentId);
+
+}
