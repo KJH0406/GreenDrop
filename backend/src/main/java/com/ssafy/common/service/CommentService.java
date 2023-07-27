5c16fb496f5a7564f338c6202b8fead7bbb6406c
kyeongin- <a27313797@gmail.com>
2023-07-27 10:03:12 +0900

    Feat/BE : 댓글 등록 , 대댓글 등록 , 댓글리스트
    - 댓글등록 , 대댓글 등록 , 댓글리스트를 위한 controller service 생성

--- backend/src/main/java/com/ssafy/common/service/CommentService.java
+++ backend/src/main/java/com/ssafy/common/service/CommentService.java
@@ -1,7 +1,7 @@
 package com.ssafy.common.service;
 
 import com.ssafy.common.dto.CommentDto;
-import com.ssafy.common.entity.Board;
+import com.ssafy.common.entity.Comment;
 import com.ssafy.common.repository.BoardRepository;
 import com.ssafy.common.repository.CommentRepository;
 import com.ssafy.common.security.Encoder;
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -25,16 +28,43 @@ public class CommentService {
 
         commentDto.setIp(ip);
         commentDto.setIsChild(0);
-        String encodepwd = encoder.encode(commentDto.getPassword());
-        commentDto.setIsDeleted(0);
-        commentDto.setNickName(commentDto.getNickName());
-        commentDto.setPassword(encodepwd);
-        log.info("boardNo 값 확인 : {} ",boardNo);
+
+        if(commentDto.getPassword() != null) {
+            String encodepwd = encoder.encode(commentDto.getPassword());
+            commentDto.setPassword(encodepwd);
+        }
         commentDto.setBoard(boardRepository.getReferenceById(boardNo));
         commentRepository.save(commentDto.toEntity());
 
+    }
+
+    @Transactional
+    public void saveChildComment(Long commentNo, String ip, CommentDto commentDto){
+
+        commentDto.setIp(ip);
+        commentDto.setIsChild(1);
+        if(commentDto.getPassword() != null) {
+            String encodepwd = encoder.encode(commentDto.getPassword());
+            commentDto.setPassword(encodepwd);
+        }
+        commentDto.setParentId(commentNo);
+        commentDto.setBoard(commentRepository.getReferenceById(commentNo).getBoard());
+        commentRepository.save(commentDto.toEntity());
 
     }
 
+        public List<CommentDto.commentList> getCommentList(Long boardNo){
 
+        List<Comment> boardComments = commentRepository.findByComment(boardNo);
+            List<CommentDto.commentList> resultList = new ArrayList<>();
+        for (Comment c : boardComments){
+            if(c.getParentId() == null) {
+                List<Comment> cocoment = commentRepository.findByParentId(c.getCommentSeq());
+                CommentDto.commentList result = new CommentDto.commentList(c,cocoment);
+                resultList.add(result);
+            }
+            else break;
+        }
+        return resultList;
+    }
 }
