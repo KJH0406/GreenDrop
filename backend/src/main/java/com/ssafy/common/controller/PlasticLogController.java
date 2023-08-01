45be7505c138946c1541a2f7b27d58e7901d513c
WonjunChun <chunjh1103@naver.com>
2023-08-01 14:09:26 +0900

    Feat/BE : PlasticLog Controller 작성
    - PatchMapping을 통해 오늘 게임의 컵 개수 갱신해 줄 updateVoteCount 작성 -> @PathVariable String direction, @RequestBody EquipmentRequestDto 형태로 데이터 받음. leftCount, rightCount 반환함.
    - GetMapping을 통해 현재까지 수거한 컵 총량 반환하는 totalPlastic 작성 -> totalCount 반환
    - GetMapping을 통해 일자별 컵 수거량을 반환하는 plasticPerDay 작성 -> @PathVariable LocalDate date 형태로 일자 받음. 해당 일자의 컵 수거 총량 반환함.

---
+++ backend/src/main/java/com/ssafy/common/controller/PlasticLogController.java
@@ -0,0 +1,49 @@
+package com.ssafy.common.controller;
+
+import com.ssafy.common.dto.request.EquipmentRequestDto;
+import com.ssafy.common.dto.response.PlasticLogResponseDto;
+import com.ssafy.common.service.GameService;
+import com.ssafy.common.service.PlasticLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RequestMapping("/plastic")
+@RestController
+public class PlasticLogController {
+    private final GameService gameService;
+    private final PlasticLogService plasticLogService;
+
+    @PatchMapping("/{direction}")
+    public ResponseEntity<PlasticLogResponseDto> updateVoteCount(
+            @PathVariable String direction,
+            @RequestBody EquipmentRequestDto equipmentRequestDto){
+        PlasticLogResponseDto response = plasticLogService.updatePlastic(direction, equipmentRequestDto);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //지금까지 수거한 컵 총량
+    @GetMapping("/list")
+    public ResponseEntity<Object> totalPlastic(){
+        Long totalPlastic = plasticLogService.getTotalPlastic();
+        Map<String, Long> result = new HashMap<>();
+        result.put("totalCount", totalPlastic);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    //일자별 수거량
+    @GetMapping("/list/{date}")
+    public ResponseEntity<Object> plasticPerDay(@PathVariable LocalDate date){
+        Integer plasticCount = plasticLogService.getPlasticCountByDate(date);
+        Map<String, Integer> result = new HashMap<>();
+        result.put("todayCount", plasticCount);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
