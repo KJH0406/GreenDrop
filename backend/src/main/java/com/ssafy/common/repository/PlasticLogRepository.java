fa79a0de172f8b57ea979d89bb9b443c1e08543d
WonjunChun <chunjh1103@naver.com>
2023-08-02 16:31:11 +0900

    Feat/BE : PlasticLog Repository에 시간대별로 수거량 조회하는 쿼리 추가
    - 시간대별로 수거량 집계하는 countByTime 추가

--- backend/src/main/java/com/ssafy/common/repository/PlasticLogRepository.java
+++ backend/src/main/java/com/ssafy/common/repository/PlasticLogRepository.java
@@ -2,7 +2,15 @@ package com.ssafy.common.repository;
 
 import com.ssafy.common.entity.PlasticLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface PlasticLogRepository extends JpaRepository<PlasticLog, Long> {
     long count();
+
+    @Query("select count(*) from PlasticLog where createdDate between :startTime and :endTime")
+    int countByTime(@Param("startTime")LocalDateTime startTime, @Param("endTime")LocalDateTime endTime);
+
 }
