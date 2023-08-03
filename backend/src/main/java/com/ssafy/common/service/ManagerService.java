d34a020587a5e6501714b9636172968de8d29b9c
WonjunChun <chunjh1103@naver.com>
2023-08-03 16:27:12 +0900

    Feat/BE : ManagerService에 관리자 삭제, 전체 조회, 관리자 비밀번호 수정 메서드 작성
    - 관리자 전체 조회하는 getAllManager 메서드 작성
    - 관리자 삭제하는 deleteManager 메서드 작성
    - 관리자 비밀번호 수정하는 changeAdminPassword 메서드 작성

--- backend/src/main/java/com/ssafy/common/service/ManagerService.java
+++ backend/src/main/java/com/ssafy/common/service/ManagerService.java
@@ -1,5 +1,6 @@
 package com.ssafy.common.service;
 
+import com.ssafy.common.dto.ManagerListDto;
 import com.ssafy.common.dto.request.ManagerRequestDto;
 import com.ssafy.common.dto.response.ManagerResponseDto;
 import com.ssafy.common.entity.Manager;
@@ -10,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
@@ -17,12 +22,11 @@ public class ManagerService {
     private final ManagerRepository managerRepository;
     private final TokenProvider tokenProvider;
 
+    //일반 ADMIN 등록
     @Transactional
     public void registManager(ManagerRequestDto request){
         Encoder encoder = new Encoder();
-//        Manager manager = managerRepository.save(request.toRegistEntity(encoder)); //패스워드 암호화를 위한 encoder 추가
         managerRepository.save(request.toRegistEntity(encoder)); //패스워드 암호화를 위한 encoder 추가
-//        return ManagerResponseDto.from(manager);
     }
 
     //TODO: 로그인 시각 DB에 저장하기
@@ -34,5 +38,41 @@ public class ManagerService {
         //토큰 생성
         String token = tokenProvider.createToken(String.format("%s:%s", manager.getId(), manager.getRole()));
         return ManagerResponseDto.from(manager, token);
+    }
+
+    public List<ManagerListDto> getAllManager(){
+        List<Manager> managerList = managerRepository.findAll();
+        List<ManagerListDto> result = new LinkedList<>();
+        for(Manager manager: managerList){
+            result.add(ManagerListDto.fromEntity(manager));
+        }
+        return result;
+    }
+
+    //삭제된 컬럼 수 반환
+    @Transactional
+    public void deleteManager(Short managerSeq){
+        managerRepository.deleteByManagerSeq(managerSeq);
+    }
+
+    @Transactional
+    public void changeAdminPassword(Map<String, String> adminInfo){
+        String id = adminInfo.get("id");
+        String password = adminInfo.get("password");
+        String newPassword = adminInfo.get("newPassword");
+
+        Encoder encoder = new Encoder();
+        Manager manager = managerRepository.findById(id)
+                .filter(it -> encoder.matches(password, it.getPassword())) //요청에 담긴 password와 암호화된 password 비교
+                .orElseThrow(()-> new IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다."));
+        if(newPassword.equals("") || newPassword == null) throw new IllegalArgumentException("새 비밀번호를 입력해 주세요.");
+        Manager newManager = Manager.builder()
+                .managerSeq(manager.getManagerSeq())
+                .id(manager.getId())
+                .password(encoder.encode(newPassword))
+                .createdDate(manager.getCreatedDate())
+                .role(manager.getRole())
+                .build();
+        managerRepository.save(newManager);
     }
 }
